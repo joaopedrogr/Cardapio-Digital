7354f1e6a901554200c600698e94dc6f626c1052
João Pedro <126629077+joaopedrogr@users.noreply.github.com>
2025-09-23 22:01:02 -0300

    fix: adjust files

--- frontend/src/hooks/useFoodData.ts
+++ frontend/src/hooks/useFoodData.ts
@@ -1,6 +1,23 @@
-export interface FoodData {
-    id?: number,
-    title: string,
-    image: string,
-    price: number
+import { useQuery } from "@tanstack/react-query";
+import axios, { type AxiosPromise } from "axios"
+import type { FoodData } from '../interface/FoodData';
+
+const API_URL = 'http://localhost:8080';
+
+const fetchData = async (): AxiosPromise<FoodData[]> => {
+    const response = axios.get(API_URL + '/food');
+    return response;
+}
+
+export function useFoodData(){
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['food-data'],
+        retry: 2
+    })
+
+    return {
+        ...query,
+        data: query.data?.data
+    }
 }
